696cb2a1b958bd36b9d546ca62f48d96cffda1fd
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-10-17 16:37:55 -0700

    final bits for nav function: linked to created header component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import AboutPage from './about/AboutPage';
 // eslint-disable-next-line import/no-unresolved
 import HomePage from './Home/HomePage';
+import Header from './common/Header';
 
 export default function App() {
     return (
       <div className="conatiner-fluid">
+        <Header />
         <Routes>
           <Route path="/about" element={<AboutPage />} />
           <Route path="/" element={<HomePage />} />

8cb373698db1563305d77bbfa8c2bdad4297efc5
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-10-31 22:01:26 -0700

    finishing up success actions and the first thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,23 @@
 import * as types from "./actionTypes";
-import * as courseApi from "../../api";
+import * as courseApi from "../../api/courseApi"; 
 
-export function createCourse(course){
-    return { type: types.CREATE_COURSE, course };
+//action creators
+export function createCourse(course) {
+  return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCoursesSuccess(courses){
+   return {type: types.LOAD_COURSES_SUCCESS, courses}
+}
+
+
+//thunks
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+        throw error
+    });
+  };
 }
